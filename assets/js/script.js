8b0aa101df47eb94c2a813abc1f5948f34f7307e
Paulina <frontend.portega@gmail.com>
2024-08-14 22:01:39 -0400

    Primer commit del proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Smooth scroll para enlaces de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Actualizar el año en el footer automáticamente
+document.addEventListener('DOMContentLoaded', function () {
+    const yearSpan = document.getElementById('year');
+    yearSpan.textContent = new Date().getFullYear();
+});
+
+// Tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
+
+// Tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('.display-4');
+    heading.classList.add('shining-text');
+});
